7a94a54f6167ec516129909b2d130069837b9cf9
Norman Schultz <39920230+normanrs@users.noreply.github.com>
2021-05-26 07:26:19 -0600

    Create post request (#2)
    * Fix random computer move assertion
    
    Because, der Norm, every once in a while the computer choice will match the test choice and it will be a tie.
    
    * Add play post method
    
    * Game play works hahahahahaha
    
    * Working POST to play game

--- lib/server.rb
+++ lib/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require_relative 'game.rb'
 
 get '/' do
   'Hello Rock/Paper/Scissors player!'
@@ -6,5 +7,18 @@ end
 
 get '/info' do
   "Welcome to the Rock/Paper/Scissors game! \n
-   Instructions to play HERE"
+   Send a game move to the '/play' endpoint as a POST \n
+   Eligible moves are: 'r' for rock, 'p' for paper, 's' for scissors
+   "
+end
+
+post '/play' do
+  game = Game.new(params['move'].downcase)
+  game.result.to_json
+end
+
+private
+
+def params
+  JSON.parse(request.body.read)
 end
