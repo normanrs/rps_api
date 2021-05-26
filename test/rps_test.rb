80fb8dd48c426a02bb15b8c82b928c68710468cb
normanrs <normanrs@live.com>
2021-05-26 06:07:11 -0600

    Testing is already overdue

--- test/rps_test.rb
+++ test/rps_test.rb
@@ -7,7 +7,16 @@ require './lib/rps.rb'
 class RpsTest < Minitest::Test
   include RockPaperScissors
 
+  def test_computer_choice
+    result = computer_choice
+    puts result
+    assert_instance_of String, result
+  end
+  
   def test_rock
+    skip
+    result = play('r')
+    assert result.include?('win'), -> {result}
   end
   
   def test_paper
