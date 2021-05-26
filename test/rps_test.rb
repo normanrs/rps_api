e5f58e26d7555cdd8eb642590274f21bd299cefa
normanrs <normanrs@live.com>
2021-05-26 06:36:47 -0600

    Adds unit tests

--- test/rps_test.rb
+++ test/rps_test.rb
@@ -13,10 +13,22 @@ class RpsTest < Minitest::Test
     assert_instance_of String, result
   end
   
-  def test_rock
-    skip
-    result = play('r')
-    assert result.include?('win'), -> {result}
+  def test_matching_move
+    move = ["r", "rock"]
+    result = play(move[0], move[1])
+    assert result.include?('tie')
+  end
+
+  def test_rock_paper
+    move = ["r", "paper"]
+    result = play(move[0], move[1])
+    assert_equal MATCHES[move], result
+  end
+
+  def test_rock_scissors
+    move = ["r", "scissors"]
+    result = play(move[0], move[1])
+    assert_equal MATCHES[move], result
   end
   
   def test_paper
