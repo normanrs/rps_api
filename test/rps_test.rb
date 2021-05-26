47efeafdae3a168c8e9d1fbd84bafea03eea32e8
normanrs <normanrs@live.com>
2021-05-26 06:43:33 -0600

    Full test coverage

--- test/rps_test.rb
+++ test/rps_test.rb
@@ -9,7 +9,7 @@ class RpsTest < Minitest::Test
 
   def test_computer_choice
     result = computer_choice
-    puts result
+    puts "Computer returned #{result}"
     assert_instance_of String, result
   end
   
@@ -31,13 +31,41 @@ class RpsTest < Minitest::Test
     assert_equal MATCHES[move], result
   end
   
-  def test_paper
+  def test_paper_rock
+    move = ["p", "rock"]
+    result = play(move[0], move[1])
+    assert_equal MATCHES[move], result
   end
 
-  def test_scissors
+  def test_paper_scissors
+    move = ["p", "scissors"]
+    result = play(move[0], move[1])
+    assert_equal MATCHES[move], result
+  end
+
+  def test_scissors_rock
+    move = ["s", "rock"]
+    result = play(move[0], move[1])
+    assert_equal MATCHES[move], result
+  end
+
+  def test_scissors_paper
+    move = ["s", "paper"]
+    result = play(move[0], move[1])
+    assert_equal MATCHES[move], result
+  end
+
+  def test_random_computer_choice
+    move = ["s"]
+    result = play(move[0])
+    puts "Outcome of actual game was: #{result}"
+    assert result.include?('win')
   end
 
   def test_bad_input
+    move = ["huh", "paper"]
+    result = play(move[0], move[1])
+    assert_equal 'Unrecognized move. Try again?', result
   end
 
 end
